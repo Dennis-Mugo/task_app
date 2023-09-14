a9345926080c5ea777c07abac99b665f931601e7
Raphael Kathambana <maya12raph@gmail.com>
2023-09-14 11:03:53 +0300

    added files and directories for assignment one

---
+++ AssignmentOne_PartOne/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Part One</title>
+</head>
+<body>
+    <h1>Part One of the Assignment</h1>
+    <p>&copy; Dennis and Hawiana</p>
+</body>
+</html>
---
+++ AssignmentOne_PartTwo/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Part One</title>
+</head>
+<body>
+    <h1>Part Two of the Assignment</h1>
+    <p>&copy; Bethelhem and Raphael</p>
+</body>
+</html>
