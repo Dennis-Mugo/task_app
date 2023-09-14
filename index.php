3c486bba3081d8bc81b88b51faac87a88893f12a
Raphael Kathambana <maya12raph@gmail.com>
2023-09-14 21:24:38 +0300

    added links to the two parts

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
     <p> What is the meaning of life? </p>
     <p> Raphael father of VS Code </p>
     <h2>Hii, I'm new here...</h2>
+
+    <a href="AssignmentOne_PartOne\index.php">Part One</a>
+    <br>
+    <a href="AssignmentOne_PartTwo\index.php">Part Two</a>
 </body>
 </html>
