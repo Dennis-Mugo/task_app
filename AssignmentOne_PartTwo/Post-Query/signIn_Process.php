d81e8fe3715e3b8092eb1bc7c6ba697ebb2dceeb
Raphael Kathambana <maya12raph@gmail.com>
2023-09-20 15:55:28 +0300

    fixed sign in process

--- AssignmentOne_PartTwo/Post-Query/signIn_Process.php
+++ AssignmentOne_PartTwo/Post-Query/signIn_Process.php
@@ -1,36 +1,32 @@
 <?php
 require_once "connection.php";
 
-
-
 // $insert_data = "INSERT Users (Username, FullName, Passwords, Email) values ('$Username', '$FullName', '$Passwords', '$Email');";
 $spot_user = "SELECT * FROM `Users`;";
-$users = $conn -> query($spot_user);
+$users = $conn->query($spot_user);
 
+// $theEmail = '';
+// $thePass = '';
 
-if ($user_row = $users -> fetch_assoc()) {
+if ($user_row = $users->fetch_assoc()) {
     $Email = $_POST["Email"];
     $Passwords = $_POST["Passwords"];
-    
-    while ($user_row = $users -> fetch_assoc()) {
-     
+
+
         $theEmail = $user_row["Email"];
         $thePass = $user_row["Passwords"];
 
-        if ($theEmail == $Email && $thePass == $Passwords){
+        if ($theEmail == $Email && $thePass == $Passwords) {
             header("Location: ../main.php");
             $displayname = $user_row["Username"];
             exit();
         }
-    }
-    if ($theEmail !== $Email && $thePass !== $Passwords){
+    
+    if ($theEmail !== $Email && $thePass !== $Passwords) {
         echo "AHHJOh";
         exit();
     }
-   
-}
-else {
-    echo $conn -> connect_error;
-}
 
-?>
+} else {
+    echo $conn->connect_error;
+}
