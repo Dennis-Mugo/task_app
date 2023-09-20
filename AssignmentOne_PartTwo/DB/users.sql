e6d8cfb6640d62d0c120f0546fc3c4dbcfb41e88
Raphael Kathambana <maya12raph@gmail.com>
2023-09-20 18:45:20 +0300

    added admin credentials to db

--- AssignmentOne_PartTwo/DB/users.sql
+++ AssignmentOne_PartTwo/DB/users.sql
@@ -1,12 +1,19 @@
 CREATE DATABASE IF NOT EXISTS `Users`;
 
+drop table if exists `Users`;
 CREATE TABLE IF NOT EXISTS `Users` (
-	`Username` varchar(15) primary key,
+    `id` int(3) NOT NULL AUTO_INCREMENT,
+	`Username` varchar(15),
     `FullName` varchar(70),
     `Email` varchar(80),
-    `Passwords` varchar(20)
+    `Passwords` varchar(20),
+    `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
 );
 
-INSERT Users (Username, FullName, Passwords, Email) values ('Cheese', 'Jerry', 'pass', 'JerryMouse@cheese.com');
+--user
+INSERT Users (Username, FullName, Passwords, Email) values ('Cheese', 'Jerry', 'pass', 'JerryMouse@cheese.com'); 
+--admin
+INSERT Users (Username, FullName, Passwords, Email, `is_admin`) values ('Cheese', 'Jerry', 'pass', 'JerryMouse@cheese.com', 1); 
 
 SELECT * FROM Users;
