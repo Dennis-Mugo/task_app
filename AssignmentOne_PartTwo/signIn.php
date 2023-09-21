1714d9753bc07f789e0a01101cd10442ab96d792
Raphael Kathambana <maya12raph@gmail.com>
2023-09-21 09:33:35 +0300

    added error messages to sign in

--- AssignmentOne_PartTwo/signIn.php
+++ AssignmentOne_PartTwo/signIn.php
@@ -19,6 +19,23 @@
         <h1><br>Login</h1>
     </div>
     <div>
+        <?php
+            if (isset($_GET['error'])) {
+                if ($_GET['error'] == "empty-fields") {
+                    echo /*html*/ '<p class="error">Fill in all fields!</p>';
+                } elseif ($_GET['error'] == "sql-error") {
+                    echo /*html*/ '<p class="error">SQL Error!</p>';
+                } elseif ($_GET['error'] == "wrong-pwd") {
+                    echo /*html*/ '<p class="error">Wrong Password!</p>';
+                } elseif ($_GET['error'] == "no-user") {
+                    echo /*html*/ '<p class="error">No User!</p>';
+                } elseif ($_GET['error'] == "form-error") {
+                    echo /*html*/ '<p class="error">No Post</p>';
+                }
+            }
+        ?>
+    </div>
+    <div>
         <form action="Post-Query/signIn_Process.php" method="POST" autocomplete="on">
             <br>
             <label>Email Address</label>
@@ -28,7 +45,7 @@
             <input name="Passwords" type="password" placeholder="Please Enter Your Password">
             <a href="forgotPass.php" class="hov" style="font-size: small;"><br><br><br><br><br>Forgot Password?</a>
             <label></label>
-            <input type="submit" class="button" value="Sign In">
+            <input type="submit" class="button" value="Sign In" name="SignIn" />
         </form>
     </div>
 
