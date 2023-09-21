641b1b7ee141c030160bcedb4b9cbad02260f667
Raphael Kathambana <maya12raph@gmail.com>
2023-09-21 11:26:22 +0300

    added password hash verification to sign in

--- AssignmentOne_PartTwo/Post-Query/signIn_Process.php
+++ AssignmentOne_PartTwo/Post-Query/signIn_Process.php
@@ -1,32 +1,73 @@
 <?php
-require_once "connection.php";
+require_once "myconnection.php";
 
-// $insert_data = "INSERT Users (Username, FullName, Passwords, Email) values ('$Username', '$FullName', '$Passwords', '$Email');";
-$spot_user = "SELECT * FROM `Users`;";
-$users = $conn->query($spot_user);
-
-// $theEmail = '';
-// $thePass = '';
-
-if ($user_row = $users->fetch_assoc()) {
+if (isset($_POST['SignIn'])) {
     $Email = $_POST["Email"];
     $Passwords = $_POST["Passwords"];
 
-
-        $theEmail = $user_row["Email"];
-        $thePass = $user_row["Passwords"];
-
-        if ($theEmail == $Email && $thePass == $Passwords) {
-            header("Location: ../main.php");
-            $displayname = $user_row["Username"];
-            exit();
-        }
-    
-    if ($theEmail !== $Email && $thePass !== $Passwords) {
-        echo "AHHJOh";
+    if (empty($Email) || empty($Passwords)) {
+        header("Location: ../signIn.php?error=empty-fields");
         exit();
-    }
+    } else {
+        $sql = "SELECT * FROM `Users` WHERE Email=?;";
+        $stmt = mysqli_stmt_init($con);
+        if (! mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signIn.php?error=sql-error");
+            exit();
+        } else {
+            mysqli_stmt_bind_param($stmt, "s", $Email);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if ($row = mysqli_fetch_assoc($result)) {
+                if (!password_verify($Passwords, $row['Passwords'])) {
+                    header("Location: ../signIn.php?error=wrong-pwd");
+                    exit();
+                } else {
+                    session_start();
+                    $_SESSION['userId'] = $row['UserId'];
+                    $_SESSION['userUid'] = $row['Username'];
 
+                    header("Location: ../main.php?login=success");
+                    exit();
+                }
+            } else {
+                header("Location: ../signIn.php?error=no-user");
+                exit();
+            }
+        }
+    }
 } else {
-    echo $conn->connect_error;
+    header("Location: ../signIn.php?error=form-error");
+    exit();
 }
+
+// // $insert_data = "INSERT Users (Username, FullName, Passwords, Email) values ('$Username', '$FullName', '$Passwords', '$Email');";
+// $spot_user = "SELECT * FROM `Users`;";
+// $users = $con->query($spot_user);
+
+// // $theEmail = '';
+// // $thePass = '';
+
+// if ($user_row = $users->fetch_assoc()) {
+//     $Email = $_POST["Email"];
+//     $Passwords = $_POST["Passwords"];
+
+
+//         $theEmail = $user_row["Email"];
+//         $thePass = $user_row["Passwords"];
+
+//         if ($theEmail == $Email && $thePass == $Passwords) {
+//             header("Location: ../main.php");
+//             $displayname = $user_row["Username"];
+//             exit();
+//         }
+
+//     if ($theEmail !== $Email && $thePass !== $Passwords) {
+//         echo "AHHJOh";
+
+//         exit();
+//     }
+
+// } else {
+//     echo $con->connect_error;
+// }
