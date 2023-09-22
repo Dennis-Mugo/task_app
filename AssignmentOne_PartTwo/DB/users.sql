dca633560f98d16e96d50d3e0907540701a5994a
BethTesHS <131249570+BethTesHS@users.noreply.github.com>
2023-09-22 08:41:00 +0300

    very small edits on the sql file

--- AssignmentOne_PartTwo/DB/users.sql
+++ AssignmentOne_PartTwo/DB/users.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
     `is_admin` tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 );
---admin
+/* admin */
 INSERT Users (Username, FullName, Passwords, Email, `is_admin`) values ('Cheese', 'Jerry', '$2y$10$Jr01rJA3zPPDC4ez0xzySO0U0MSn2toU3t/7wPaHhcCbI04c1/qlu', 'JerryMouse@cheese.com', 1); 
 
 SELECT * FROM Users;
@@ -28,4 +28,4 @@ CREATE TABLE IF NOT EXISTS `keys` (
 
 INSERT into `keys` (`key`, `user_id`) values ('1234567890', 1);
 
-SELECT * FROM keys;
+SELECT * FROM `keys`;
