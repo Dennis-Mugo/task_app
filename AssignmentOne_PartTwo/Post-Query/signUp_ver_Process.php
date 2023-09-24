fae1bfafa57476a4042af378c10fbdd304e2f312
Raphael Kathambana <maya12raph@gmail.com>
2023-09-24 18:06:57 +0300

    updated key verification

--- AssignmentOne_PartTwo/Post-Query/signUp_ver_Process.php
+++ AssignmentOne_PartTwo/Post-Query/signUp_ver_Process.php
@@ -1,9 +1,14 @@
 <?php
 require_once "myconnection.php";
 
-if(isset($_POST["Verify"])){
+if (isset($_POST["Verify"])) {
     $VerCode = $_POST["Code"];
     $userId = $_POST['user-id'];
+
+    if (isset($_GET['user-id'])) {
+        $userId = $_GET['user-id'];
+    }
+
 
     $sql = "SELECT * FROM `keys` WHERE `user_id` = $userId;";
     $connection = new MysqliConnection();
@@ -11,9 +16,13 @@ if(isset($_POST["Verify"])){
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $code = $row['key'];
-    }
 
-    if($VerCode == $code){
-        header("Location: ../index.php");
+        if ($VerCode == $code) {
+            header("Location: ../index.php");
+        } else {
+            header("location: ../SignUp_Verify.php?error=invalid-key&user-id=" . $userId);
+        }
     }
+} else {
+    header("location: ../SignUp_Verify.php?error=unknown");
 }
--- AssignmentOne_PartTwo/SignUp_Verify.php
+++ AssignmentOne_PartTwo/SignUp_Verify.php
@@ -4,7 +4,7 @@
 <head>
     <?php
     require_once "aDoc.php"
-    ?>
+        ?>
 </head>
 
 <body>
@@ -17,10 +17,23 @@
         <h1><br>Enter Verification Code</h1>
     </div>
     <div>
+        <div>
+            <?php
+            if (isset($_GET['error'])) {
+                if ($_GET['error'] == "empty-fields") {
+                    echo /*html*/'<p class="error">Fill in all fields!</p>';
+                } elseif ($_GET['error'] == "invalid-key") {
+                    echo /*html*/'<p class="error">Invalid key</p>';
+                } elseif ($_GET['error'] == "unknown") {
+                    echo /*html*/'<p class="error">Something went wrong. <br> Please try again.</p>';
+                }
+            }
+            ?>
+        </div>
         <form action="Post-Query/signUp_ver_Process.php" method="POST" autocomplete="off">
             <br>
             <label>Your verification code has been sent to your email.</label>
-            <input name="Code" type="text" placeholder="Please Enter Your Verification Code">
+            <input required name="Code" type="text" placeholder="Please Enter Your Verification Code">
             <input type="hidden" name="user-id" value="<?php echo $_GET['user-id']; ?>">
             <input name="Verify" type="submit" class="button" value="Enter">
             <div class="footer">
